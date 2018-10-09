1ab9fc55dd802712390fe87ee10ff6094ced2a00
BruceZhou123 <2731760609@qq.com>
2018-10-09 16:48:51 +0800

    decorator

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 
 ## 1.fun.py 4种函数接收参数方式
 ## 2.lambda匿名函数
+## 3.closure(闭包函数)
+## 4.decorator(装饰器函数)
---
+++ decorator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#_*_coding=utf-8_*_
+
+
+from time import ctime
+
+
+def printTime(func):
+    def wrappendfunc():
+        print "The func name is %s" % (func.__name__)
+        print "The time is %s" % (ctime())
+        return func()
+    return wrappendfunc
+
+
+@printTime
+def hi():
+    print "hi ,world!"
+
+hi()
